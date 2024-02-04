0195691627bae60544ef73f8b85d778b39e40427
Kirthana <131602714+Kirthana03@users.noreply.github.com>
2024-02-04 17:20:09 +0530

    Update thumbstick-logging.js

--- thumbstick-logging.js
+++ thumbstick-logging.js
@@ -5,14 +5,14 @@ AFRAME.registerComponent('thumbstick-logging',{
   logThumbstick: function (evt) {
 
    let camPos = document.querySelector('#cam');
-   let pos =  camPos.getAttribute('position');
-   let posX = pos['x'] 
-   let posY = pos['y'] 
-   let posZ = pos['z'];
+   // let pos =  camPos.getAttribute('position');
+   // let posX = pos['x'] 
+   // let posY = pos['y'] 
+   // let posZ = pos['z'];
     if (evt.detail.y > 0.95) { 
-      posZ = posZ - 0.5;
-      console.log(posZ);
-      camPos.setAttribute('position', `${posX}  ${posY}  ${posZ}`);
+      // posZ = posZ - 0.5;
+      // console.log(posZ);
+      camPos.setAttribute('position', {x: 0, y: 3, z: 10});
       }
     if (evt.detail.y < -0.95) { console.log("UP"); }
     if (evt.detail.x < -0.95) { console.log("LEFT"); }
