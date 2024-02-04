df9efed665a408c980bac0875d97cf58664b9cbd
Kirthana <131602714+Kirthana03@users.noreply.github.com>
2024-02-04 17:00:18 +0530

    Update thumbstick-logging.js

--- thumbstick-logging.js
+++ thumbstick-logging.js
@@ -1,22 +1,21 @@
 AFRAME.registerComponent('thumbstick-logging',{
-    init: function () {
-      this.el.addEventListener('thumbstickmoved', this.logThumbstick);
-    },
-    logThumbstick: function (evt) {
+  init: function () {
+    this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+  },
+  logThumbstick: function (evt) {
 
-     let camPos = document.querySelector('#cam');
-     let pos =  camPos.getAttribute('position');
-     let posZ = pos['z'];
-      if (evt.detail.y > 0.95) { 
-        // console.log("DOWN"); 
-        while(evt.detail.y > 0.95) {
-        posZ = posZ + 0.5;
-        console.log(posZ);
-        camPos.setAttribute('position', `${posZ}`);
-        }
-        }
-      if (evt.detail.y < -0.95) { console.log("UP"); }
-      if (evt.detail.x < -0.95) { console.log("LEFT"); }
-      if (evt.detail.x > 0.95) { console.log("RIGHT"); }
-    }
-  });
+   let camPos = document.querySelector('#cam');
+   let pos =  camPos.getAttribute('position');
+   let posX = pos['x'] 
+   let posY = pos['y'] 
+   let posZ = pos['z'];
+    if (evt.detail.y > 0.95) { 
+      posZ = posZ - 0.5;
+      console.log(posZ);
+      camPos.setAttribute('position', `${posX}  ${posY}  ${posZ}`);
+      }
+    if (evt.detail.y < -0.95) { console.log("UP"); }
+    if (evt.detail.x < -0.95) { console.log("LEFT"); }
+    if (evt.detail.x > 0.95) { console.log("RIGHT"); }
+  }
+});
